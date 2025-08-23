39bd9871731e04c6a16460448ca5659c963f1224
unknown <sulthon.abady28@gmail.com>
2025-08-24 06:54:27 +0700

    refactor(criteria): implement view dto and selective query in repository

--- app/Repositories/CriterionRepository.php
+++ app/Repositories/CriterionRepository.php
@@ -2,10 +2,39 @@
 
 namespace App\Repositories;
 
+use App\Data\Criterion\CriterionCreateData;
+use App\Data\Criterion\CriterionUpdateData;
+use App\Data\Criterion\CriterionViewData;
+use App\Models\Criterion;
+use Illuminate\Support\Collection;
+
 class CriterionRepository
 {
-    public function getAllForIndex()
+    public function getAllForIndex(): Collection
     {
         $criteria = Criterion::select('id', 'name', 'description')->get();
+
+        return $criteria->map(fn($criterion) => CriterionViewData::fromModel($criterion));
+    }
+
+    public function create(CriterionCreateData $data): Criterion
+    {
+        return Criterion::create([
+            'name' => $data->name,
+            'description' => $data->description,
+        ]);
+    }
+
+    public function update(Criterion $criterion, CriterionUpdateData $data): bool
+    {
+        return $criterion->update([
+            'name' => $data->name,
+            'description' => $data->description,
+        ]);
+    }
+
+    public function delete(Criterion $criterion): bool
+    {
+        return $criterion->delete();
     }
 }

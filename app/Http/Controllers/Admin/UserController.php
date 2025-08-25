3a430f098648467fb38a157800a89481e6b88cdd
unknown <sulthon.abady28@gmail.com>
2025-08-26 05:40:17 +0700

    feat(users): implement full crud logic in UserController

---
+++ app/Http/Controllers/Admin/UserController.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Data\User\UserViewData;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Admin\StoreUserRequest;
+use App\Http\Requests\Admin\UpdateUserRequest;
+use App\Models\User;
+use App\Repositories\UserRepository;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function __construct(
+        private readonly UserRepository $userRepository
+    ) {}
+
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $users = $this->userRepository->getAllForIndex();
+
+        return view('admin.users.index', compact('users'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('admin.users.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreUserRequest $request)
+    {
+        $this->userRepository->create($request->toDto());
+
+        return redirect()->route('admin.users.index')->with('success', 'Pengguna baru berhasil ditambahkan.');
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(User $user)
+    {
+        $userData = UserViewData::fromModel($user);
+
+        return view('admin.users.edit', ['user' => $userData]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateUserRequest $request, User $user)
+    {
+        $this->userRepository->update($user, $request->toDto());
+
+        return redirect()->route('admin.users.index')->with('success', 'Data pengguna berhasil diperbarui.');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(User $user)
+    {
+        $this->userRepository->delete($user);
+
+        return redirect()->route('admin.users.index')->with('success', 'Pengguna berhasil dihapus.');
+    }
+}

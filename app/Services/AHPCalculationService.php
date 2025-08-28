aca76bbd43a3e9bb7e69d0d2ab8beab81af5451f
unknown <sulthon.abady28@gmail.com>
2025-08-29 02:59:39 +0700

    feat(ahp): implement geometric mean aggregation in calculation service

--- app/Services/AHPCalculationService.php
+++ app/Services/AHPCalculationService.php
@@ -4,6 +4,7 @@ namespace App\Services;
 
 use App\Models\Period;
 use App\Repositories\ComparisonRepository;
+use Illuminate\Support\Collection;
 
 class AHPCalculationService
 {
@@ -16,7 +17,6 @@ class AHPCalculationService
      */
     public function calculateForPeriod(Period $period): void
     {
-        // TODO:
         // 1. Ambil semua data penilaian dari repository.
         $allInputs = $this->comparisonRepository->getAllPeriod($period->id);
 
@@ -24,8 +24,67 @@ class AHPCalculationService
             return;
         }
 
+        $criteriaIds = $period->criteria()->pluck('id');
+        $alternativeIds = $period->alternatives()->pluck('id');
+
         // 2. Lakukan agregasi dengan Rata-rata Geometrik.
+        $criteriaInputs = $allInputs->where('comparison_type', 'criterion');
+        $aggregatedCriterianMatrix = $this->aggregateMatrix($criteriaInputs, $criteriaIds);
+
         // 3. Lakukan perhitungan AHP.
+        $aggreagatedAlternativeMatrices = [];
+        foreach ($criteriaIds as $criterionId) {
+            $alternativeInputs = $allInputs
+                ->where('comparison_type', 'alternative')
+                ->where('criterion_id', $criterionId);
+
+            $aggregatedCriterianMatrix = $this->aggregateMatrix($alternativeInputs, $alternativeIds);
+        }
+
         // 4. Simpan hasilnya ke database.
+    }
+
+    /**
+     * Mengagregasi sekumpulan input perbandingan menjadi satu matriks final menggunakan rata-rata geometrik
+     * @param \Illuminate\Support\Collection $inputs
+     * @param \Illuminate\Support\Collection $itemIds
+     * @return array<array>
+     */
+    private function aggregateMatrix(Collection $inputs, Collection $itemIds): array
+    {
+        $matrix = [];
+        $itemIdsArray = $itemIds->toArray();
+
+        foreach ($itemIdsArray as $rowId) {
+            foreach ($itemIdsArray as $colId) {
+                if ($rowId === $colId) {
+                    $matrix[$rowId][$colId] = 1.0;
+                }
+
+                $pairInputs = $inputs->filter(function ($item) use ($rowId, $colId) {
+                    return ($item->item1_id == $rowId && $item->item2_id == $colId) || ($item->item1_id == $colId && $item->item2_id == $rowId);
+                });
+
+                if ($pairInputs->isEmpty()) {
+                    $matrix[$rowId][$colId] = 1.0;
+                    continue;
+                }
+
+                $product = 1.0;
+                foreach ($pairInputs as $input) {
+                    $value = (float) $input->value;
+                    if ($input->item1_id == $colId && $input->item2_id == $rowId) {
+                        $product *= (1 / $value);
+                    } else {
+                        $product *= $value;
+                    }
+                }
+
+                $geomtricMean = pow($product, 1.0 / $pairInputs->count());
+                $matrix[$rowId][$colId] = $geomtricMean;
+            }
+        }
+
+        return $matrix;
     }
 }
